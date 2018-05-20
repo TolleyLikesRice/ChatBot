03aec26424db4f4a1b3013481421f5a0f997f484
TolleyB-J <39429061+TolleyB-J@users.noreply.github.com>
2018-05-20 11:13:23 +0100

    Bug Fixes

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 const chalk = require('chalk');
-module.exports = client => {
-    client.user.setActivity('Type ;help', { type: 'PLAYING' });
+module.exports = (client, guild) => {
     console.log(chalk.green('Connected!'));
+    client.user.setActivity('Type ;help', { type: 'PLAYING' });
 };
