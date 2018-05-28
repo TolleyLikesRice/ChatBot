ce6966b06a0e90f8e75c303d4a9f58c0d1ad146e
Tolley B-J <39429061+TolleyB-J@users.noreply.github.com>
2018-05-28 20:01:50 +0100

    Bug Fixes

--- test/boot.js
+++ test/boot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const settings = require('./example.json');
+const settings = require('../settings.json');
 const chalk = require('chalk');
 const fs = require('fs');
 const moment = require('moment');
