aff32a6f7c079a658eb6b74ad3405cad84897068
Tolley B-J <39429061+TolleyB-J@users.noreply.github.com>
2018-05-28 19:55:31 +0100

    Fixed Test

--- test/boot.js
+++ test/boot.js
@@ -1,13 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const settings = require('../settings.json');
+const settings = require('./example.json');
 const chalk = require('chalk');
 const fs = require('fs');
 const moment = require('moment');
 const appjs = require('../app.js')
 //require('../util/eventLoader')(client);
-
-var expect = require('chai').expect;
 
 describe('#Boot', function() {
   it('Initalize', function() {
@@ -24,13 +22,6 @@ describe('#Boot', function() {
 
   it('Define Logs', function() {
     appjs.logtest()
-  });
-  it('Login', function() {
-    function readytest() {
-      console.log('Ready Test')
-      process.exit()
-    }
-    client.login(settings.token);
-    client.on('ready', () => readytest());
+    process.exit()
   });
 });
