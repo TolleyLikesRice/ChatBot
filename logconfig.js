0eaea898aa891766d721d3981c9b2c722ea75111
Tolley B-J <39429061+TolleyB-J@users.noreply.github.com>
2018-05-29 07:57:49 +0100

    Bug Fixes/Tranfering to Winston

--- logconfig.js
+++ logconfig.js
@@ -3,15 +3,7 @@ const path = require('path')
 const winstonConf = require('winston-config');
 
 if (settings.debuglogging = false) {
-    winstonConf.fromFile(path.join(__dirname, 'logconfigs/reg.json'), callback(error, winston), {
-        if(error) {
-            console.log('error during winston configuration');
-        }
-    });
+    winstonConf.fromFile(path.join(__dirname, 'logconfigs/reg.json'));
 } else {
-    winstonConf.fromFile(path.join(__dirname, 'logconfigs/debug.json'), callback(error, winston), {
-        if(error) {
-            console.log('error during winston configuration');
-        }
-    });
+    winstonConf.fromFile(path.join(__dirname, 'logconfigs/debug.json'));
 }
