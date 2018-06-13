6df2716e703f3bcae75769a1fd6954df1a60f859
Tolley B-J <39429061+TolleyB-J@users.noreply.github.com>
2018-06-13 18:17:59 +0100

    Remove Giphy testing

--- test/giphy.js
+++
@@ -1,6 +0,0 @@
-const giphy = require('../defs/definetests').giphy
-describe('Giphy Testing', function () {
-    it('Require', function () {
-        const giphy = require('../defs/definegiphy').giphy
-    });
-});
