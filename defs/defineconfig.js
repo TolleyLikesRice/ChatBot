b2e5121912aa520fddd182476a7bea577b6f6ae3
Tolley B-J <39429061+TolleyB-J@users.noreply.github.com>
2018-06-23 14:39:03 +0100

    Removed Uneccecry files & Started Per-Server-Database with firebase & Imporved Code Style

--- defs/defineconfig.js
+++
@@ -1,4 +0,0 @@
-const toml = require('toml')
-const fs = require('fs')
-const config = toml.parse(fs.readFileSync('./config.toml', 'utf-8'));
-module.exports.config = config;
--- defs/definegiphy.js
+++
@@ -1,5 +0,0 @@
-const config = require('./defineconfig').config
-const GphApiClient = require('giphy-js-sdk-core')
-const giphy = GphApiClient(config.Giphy.apikey)
-
-module.exports.giphy = giphy
