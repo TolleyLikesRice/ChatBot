6c1464bf2ba60671256cbb5d6ad87a8cdd29eb47
Ptolemy Brooks-Johnson <tolleybjcoding@gmail.com>
2019-02-20 18:53:56 +0000

    Starting to write tests for the events

--- events/disconnect.js
+++ events/disconnect.js
@@ -1,5 +1,5 @@
 const winston = require('winston');
 const prolog = winston.loggers.get('prolog');
 module.exports = () => {
-  prolog.warn('You have been disconected at ' + new Date());
+  prolog.warn('You have been disconnected at ' + new Date());
 };
---
+++ test/events.js
@@ -0,0 +1,22 @@
+/* eslint-env node, mocha */
+const expect = require('chai').expect;
+const winston = require('winston');
+const capcon = require('capture-console');
+const config = require('../mainDefs').config;
+const loginit = require('../start_scripts/loginit');
+
+require('../start_scripts/loginit');
+if (config.Logging.debug === true) {
+  describe('Events with debug enabled', function () {
+  });
+} 
+if (config.Logging.debug === false) {
+  describe('Events with debug disabled', function () {
+    it('Prints an warning when disconnect.js is called', function () {
+      var stderr = capcon.captureStderr(function scope() {
+        require('../events/disconnect')
+      });
+      expect(stderr).to.contain('[MAIN] You have been disconnected at');
+    });
+  });
+}
