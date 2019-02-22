35154f6a40a26a84bc95a4102dd303f07e8b2c8a
Ptolemy Brooks-Johnson <tolleybjcoding@gmail.com>
2019-02-22 09:41:49 +0000

    Putting reply back

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,7 +1,7 @@
 exports.run = (client, message) => {
   var responces = ['It is certain', 'Its is decidedly so', 'Without a doubt', 'Yes definatly', 'You may rely on it', 'You can count on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Absolutley', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful', 'Chances aren\'t good'];
   var show = responces[Math.floor(Math.random() * responces.length)];
-  message.channel.send(show);
+  message.reply(show);
 };
 
 exports.conf = {
