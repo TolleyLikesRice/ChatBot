5b25049b8a38299057b048153c66674b6b40eb47
Ptolemy Brooks-Johnson <tolleybjcoding@gmail.com>
2019-02-23 08:27:33 +0000

    Fixing the gitignore so the web server gets commited

---
+++ start_scripts/web.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/onlineCheck', (req, res) => res.send('ChatBot Running and functioning!'));
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
