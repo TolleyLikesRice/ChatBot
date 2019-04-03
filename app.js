133bd64d0929641ec33e815094dbd046e5a705a2
Ptolemy Brooks-Johnson <tolleybjcoding@gmail.com>
2019-04-03 10:02:17 +0100

    fixing permlvl

--- app.js
+++ app.js
@@ -79,13 +79,14 @@ function ele() {
             const admin = message.guild.roles.find(role => role.name === client.settings.get(message.member.guild.id, 'adminRole'));
             let permlvl = 1;
             if (message.author.id === config.Bot.ownerid) permlvl = 4;
+            if (message.member.id == client.settings.get(message.member.guild.id, 'serverOwner')) permlvl = 4;
             if (lvl > 1 && permlvl === 1) {
                 if (mod == undefined) {
-                    message.reply('Sorry! We could not find the moderator role on your server, please ask one of your server admins to fix this by creating a role called Moderator and then set the name of the Admin role you normally use with the setconf command');
+                    message.reply(`Sorry! We could not find the moderator role on your server, please ask one of your server admins to fix this by creating a role called ${client.settings.get(message.member.guild.id, 'modRole')} and then set the name of the Admin role you normally use with the setconf command`);
                     return 'fail';
                 }
                 if (admin == undefined) {
-                    message.reply('Sorry! We could not find the administrator role on your server, please ask one of your server admins to fix this by creating a role called Administrator and then set the name of the Admin role you normally use with the setconf command');
+                    message.reply(`Sorry! We could not find the administrator role on your server, please ask one of your server admins to fix this by creating a role called ${client.settings.get(message.member.guild.id, 'adminRole')} and then set the name of the Admin role you normally use with the setconf command`);
                     return 'fail';
                 }
                 if (message.member.roles.has(mod.id)) permlvl = 2;
