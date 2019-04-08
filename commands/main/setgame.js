580147194bfa3e8d674ced9c5af69488e2872ce9
Ptolemy Brooks-Johnson <tolleybjcoding@gmail.com>
2019-04-08 18:02:36 +0100

    Disable SetGame

--- commands/main/setgame.js
+++ commands/main/setgame.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
 };
 
 exports.conf = {
-    enabled: true,
+    enabled: false,
     guildOnly: false,
     aliases: [],
     permLevel: config.Botcmd.setgamelevel
