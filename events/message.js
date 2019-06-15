31ca57bc48ac21b3952af16f508612995e69cc13
Ptolemy Brooks-Johnson <tolleybjcoding@gmail.com>
2019-06-15 08:27:48 +0100

    Making cmd.conf.enabled actually enable/disable the command

--- events/message.js
+++ events/message.js
@@ -81,8 +81,10 @@ module.exports = message => {
         cmd = client.commands.get(command);
     } else if (client.aliases.has(command)) {
         cmd = client.commands.get(client.aliases.get(command));
+    } else if (cmd.conf.enabled === false) {
+        return message.reply(':frowning: Sorry, that command is disabled.');
     } else {
-        return message.reply(':frowning: I don\'t recognize that command. Do ' + prefix + 'help to see all of my commands. If you believe this is in error then dm <@251055152667164676>');
+        return message.reply(':frowning: I don\'t recognize that command. Do `' + prefix + 'help` to see all of my commands. If you believe this is in error then dm <@251055152667164676>');
     }
     const perms = client.elevation(message, cmd.conf.permLevel);
     if (perms === 'fail') return;
